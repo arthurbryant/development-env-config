e0d85c0481d369a4725f51912fa07e79ab3a4187
feng.zhang <cst.feng@gmail.com>
2018-03-16 15:49:57 +0900

    add initial script

--- initial.sh
+++ initial.sh
@@ -2,5 +2,15 @@
 
 mkdir -p ~/.initial-config
 cd ~/.initial-config
-git clone git@github.com:arthurbryant/linux-initial-config.git
+if [ -e ~/.initial-config/linux-initial-config ]; then
+    echo "Git clone already done."
+else
+    git clone git@github.com:arthurbryant/linux-initial-config.git
+fi
 
+ln -sf ~/.initial-config/linux-initial-config/.bashrc-all ~/.bashrc-all
+mkdir -p ~/.bashrc-config
+ln -sf ~/.initial-config/linux-initial-config/.bashrc-config/* ~/.bashrc-config/
+
+# FIXME: check this line exist at .bashrc or not before add it.
+echo "\nsource ~/.bashrc-all" >> ~/.bashrc
